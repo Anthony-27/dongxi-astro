2ff546ba8e8058ce1ca2be3461ab28872443bf94
DONGXI <hello@dongxi.io>
2026-05-21 19:38:55 +0800

    fix: hardcode sanity projectId as fallback

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-  projectId: import.meta.env.SANITY_PROJECT_ID,
+  projectId: import.meta.env.SANITY_PROJECT_ID ?? '0brclyd6',
   dataset:   import.meta.env.SANITY_DATASET ?? 'production',
   apiVersion: '2024-01-01',
   useCdn: true,
